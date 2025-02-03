31a798b305a7858109447ee41e74efa7ab60fc9f
Sven Rogge <sreglitzki@gmail.com>
2025-02-03 09:54:47 +0100

    Prevent unnecessary execution triggers

--- lib/queue.ts
+++ lib/queue.ts
@@ -35,8 +35,8 @@ export class Queue {
    */
   parallelize: number;
   #queue = [] as [Task<any>, PositionListener?][];
-  #running: Promise<any>[] = [];
-
+  #runningTasks: Promise<any>[] = [];
+  #running = false;
   /**
    * Creates a new queue.
    *
@@ -49,17 +49,32 @@ export class Queue {
   }
 
   /**
-   * The number of jobs currently in the queue.
+   * The number of tasks currently in the queue.
    */
   get size(): number {
     return this.#queue.length;
   }
 
   /**
-   * Pushes a job to the queue.
+   * Whether the queue is currently running.
+   */
+  get running(): boolean {
+    return this.#running;
+  }
+
+  /**
+   * The number of tasks currently running.
+   */
+  get runningCount(): number {
+    return this.#runningTasks.length;
+  }
+
+  /**
+   * Pushes a task to the queue.
    *
-   * @param task - The job to push to the queue.
-   * @returns A promise that resolves to the result of the job or `null` if the queue is full.
+   * @param task - The task to push to the queue.
+   * @param positionListener - A listener that is called every time the queue position of the task changes.
+   * @returns A promise that resolves to the result of the task or `null` if the queue is full.
    */
   push<T = void>(
     task: Task<T>,
@@ -70,12 +85,14 @@ export class Queue {
       const wrapper = () => task().then(resolve, reject);
       this.#queue.push([wrapper, positionListener]);
       positionListener?.(this.#queue.length);
-      if (this.#running.length === 0) this.#executeNext();
+      if (this.#runningTasks.length === 0) this.#executeNext();
     });
     return p;
   }
 
   async #executeNext() {
+    if (this.#running) return;
+    this.#running = true;
     while (this.#queue.length > 0) {
       // Defer execution to pick up all synchronously enqueued tasks.
       await new Promise((resolve) => setTimeout(resolve));
@@ -83,16 +100,19 @@ export class Queue {
       // Execute as many tasks as possible in parallel.
       while (
         this.#queue.length > 0 &&
-        this.#running.length < this.parallelize
+        this.#runningTasks.length < this.parallelize
       ) {
         const [task] = this.#queue.shift()!;
         const res = task();
-        res.finally(() => this.#running.splice(this.#running.indexOf(res), 1));
-        this.#running.push(res);
+        res.finally(() =>
+          this.#runningTasks.splice(this.#runningTasks.indexOf(res), 1)
+        );
+        this.#runningTasks.push(res);
       }
       for (let i = 0; i < this.#queue.length; i++) this.#queue[i][1]?.(i + 1);
       // Wait for one of the current tasks to finish before executing the next one(s).
-      await Promise.race(this.#running);
+      await Promise.race(this.#runningTasks);
     }
+    this.#running = false;
   }
 }
